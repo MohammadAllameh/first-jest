9aed5cfb5c0c5c7d8acb4df19b8ee359a42a3a61
MohammadAllameh <rw1387rw@gmail.com>
2024-12-08 23:39:01 +0330

    test scribe

---
+++ src/sum.js
@@ -0,0 +1,8 @@
+function gta(c, d) {
+  //   if (c > d) return true;
+  //   if (c < d) return false;
+  //   if (c == d) return true;
+  return c >= d ? true : false;
+}
+
+module.exports = gta;
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,1 +1,25 @@
-test("");
+const gta = require("../src/sum");
+// test("our first test", () => {
+//   throw new Error("test failed");
+// });
+
+describe("gta - math pointer", () => {
+  //   test("should return true if c is grater than d", () => {
+  it("should return true if c is grater than d", () => {
+    const result = gta(3, 2);
+    //   expect(result).toBeNull();
+
+    expect(result).toBe(true);
+    expect(0.4).toBeCloseTo(0.4);
+  });
+
+  it("should return false if c is less than d", () => {
+    const result = gta(2, 3);
+    expect(result).toBe(false);
+  });
+
+  it("should return true if c is equal tab", () => {
+    const result = gta(3, 3);
+    expect(result).toBe(true);
+  });
+});
